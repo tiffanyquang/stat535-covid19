8eae2580a82ce381db08b9639d69ac2aaee4ab40
Aubrey Wiederin <AMWiederin@gmail.com>
2020-04-23 19:53:10 -0400

    Estimation of log(beta) per-state.

--- Parameters Estimation/main.py
+++ Parameters Estimation/main.py
@@ -24,9 +24,9 @@ data_cols = dates.strftime('%#m/%#d/%y').tolist()
 raw_confirmed = pd.read_csv('../JHData/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv', usecols=['Province_State']+data_cols, parse_dates=True)
 raw_deaths = pd.read_csv('../JHData/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_US.csv', usecols=['Province_State']+data_cols)
 
-
 confirmed = raw_confirmed.groupby(['Province_State']).sum().T
 deaths = raw_deaths.groupby(['Province_State']).sum().T
+
 
 death_fraction = 0.01
 recovered = deaths*(1-death_fraction)
@@ -41,9 +41,10 @@ beta[beta == np.inf] = np.NaN
 with np.errstate(divide='ignore', invalid='ignore'):
     beta_log = np.log(beta)
 beta_log[~np.isfinite(beta_log)] = np.NaN
-beta_log_mean = np.nanmean(beta_log.values.flat)
-beta_log_std = np.nanstd(beta_log.values.flat)
-beta_log_sem = stats.sem(beta_log.values.flat, nan_policy='omit')
+beta_log_stats = beta_log.agg(['mean','sem','std']).T
+beta_log_all_mean = np.nanmean(beta_log.values.flat)
+beta_log_all_std = np.nanstd(beta_log.values.flat)
+beta_log_all_sem = stats.sem(beta_log.values.flat, nan_policy='omit')
 
 # ax = beta.plot.bar(yerr=beta_std)
 # ax.set_xticks(range(len(beta)))
@@ -52,14 +53,15 @@ beta_log_sem = stats.sem(beta_log.values.flat, nan_policy='omit')
 # fig.autofmt_xdate()
 # ax.plot(confirmed)
 
-print('Estimate of log(beta):', format_sci(beta_log_mean, beta_log_sem))
-print('Standard deviation:', beta_log_std)
+print('Estimate of log(beta):', format_sci(beta_log_all_mean, beta_log_all_sem))
+print('Standard deviation:', beta_log_all_std)
+print(beta_log_stats)
 
 beta_log_valid = beta_log.values.flat
 beta_log_valid = beta_log_valid[~np.isnan(beta_log_valid)]
 
 plt.hist(beta_log_valid, bins=100, density=True)
 x = np.linspace(beta_log_valid.min(), beta_log_valid.max(), 300)
-y = np.exp(-(x-beta_log_mean)**2/(2*beta_log_std**2))/(np.sqrt(2*np.pi)*beta_log_std)
+y = np.exp(-(x-beta_log_all_mean)**2/(2*beta_log_all_std**2))/(np.sqrt(2*np.pi)*beta_log_all_std)
 plt.plot(x, y)
 plt.show()
