f939b0457c0ab2ff26c64f5d20149552069df1eb
Aubrey Wiederin <AMWiederin@gmail.com>
2020-04-28 15:30:05 -0400

    Updated Parameters Estimation to export beta_log.csv and many plots. TODO: Goodness-of-fit. Updated SIRT to use values for beta from Parameters Estimation. TODO: Check if those values are actually on the correct scale for the simulation.

--- Parameters Estimation/main.py
+++ Parameters Estimation/main.py
@@ -15,17 +15,20 @@ def format_sci(x, s, ndig=2):
     return '{}{}.{}({})'.format('-'*neg, rxi.rjust(1,'0'), rxf, rs)
 
 
+abbr = pd.read_csv('states.csv', index_col=0)
+
+
 dates = pd.date_range(start='2020-01-22', end='2020-04-22')
 
 raw_population = pd.read_csv('../JHData/csse_covid_19_data/UID_ISO_FIPS_LookUp_Table.csv', usecols=['Country_Region', 'Province_State', 'Admin2', 'Population'])
-population = raw_population[(raw_population['Country_Region'] == 'US') & (~pd.notna(raw_population['Admin2'])) & pd.notna(raw_population['Province_State']) & pd.notna(raw_population['Population'])].set_index('Province_State')['Population']
+population = abbr.merge(raw_population[(raw_population['Country_Region'] == 'US') & (~pd.notna(raw_population['Admin2'])) & pd.notna(raw_population['Province_State']) & pd.notna(raw_population['Population'])], left_on='Name', right_on='Province_State').set_index('State')['Population']
 
 data_cols = dates.strftime('%#m/%#d/%y').tolist()
-raw_confirmed = pd.read_csv('../JHData/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv', usecols=['Province_State']+data_cols, parse_dates=True)
-raw_deaths = pd.read_csv('../JHData/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_US.csv', usecols=['Province_State']+data_cols)
+raw_confirmed = abbr.merge(pd.read_csv('../JHData/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv', usecols=['Province_State']+data_cols, parse_dates=True), left_on='Name', right_on='Province_State').set_index('State')[data_cols]
+raw_deaths = abbr.merge(pd.read_csv('../JHData/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_US.csv', usecols=['Province_State']+data_cols, parse_dates=True), left_on='Name', right_on='Province_State').set_index('State')[data_cols]
 
-confirmed = raw_confirmed.groupby(['Province_State']).sum().T
-deaths = raw_deaths.groupby(['Province_State']).sum().T
+confirmed = raw_confirmed.groupby(['State']).sum().T
+deaths = raw_deaths.groupby(['State']).sum().T
 
 
 death_fraction = 0.01
@@ -42,20 +45,16 @@ with np.errstate(divide='ignore', invalid='ignore'):
     beta_log = np.log(beta)
 beta_log[~np.isfinite(beta_log)] = np.NaN
 beta_log_stats = beta_log.agg(['mean','sem','std']).T
+beta_log_stats.index.set_names('State', inplace=True)
 beta_log_all_mean = np.nanmean(beta_log.values.flat)
 beta_log_all_std = np.nanstd(beta_log.values.flat)
 beta_log_all_sem = stats.sem(beta_log.values.flat, nan_policy='omit')
 
-# ax = beta.plot.bar(yerr=beta_std)
-# ax.set_xticks(range(len(beta)))
-# ax.set_xticklabels(beta.index, rotation=90)
-# fig, ax = plt.subplots()
-# fig.autofmt_xdate()
-# ax.plot(confirmed)
 
 print('Estimate of log(beta):', format_sci(beta_log_all_mean, beta_log_all_sem))
 print('Standard deviation:', beta_log_all_std)
 print(beta_log_stats)
+beta_log_stats.to_csv('out/beta_log.csv')
 
 beta_log_valid = beta_log.values.flat
 beta_log_valid = beta_log_valid[~np.isnan(beta_log_valid)]
@@ -67,5 +66,19 @@ x = np.linspace(beta_log_valid.min(), beta_log_valid.max(), 300)
 y = np.exp(-(x-beta_log_all_mean)**2/(2*beta_log_all_std**2))/(np.sqrt(2*np.pi)*beta_log_all_std)
 ax.plot(x, y)
 ax.set_xlabel(r'$\log(\beta)$')
-fig.savefig('beta_hist.pdf', bbox_inches='tight')
-fig.savefig('beta_hist.png', bbox_inches='tight')
+fig.savefig('out/beta_hist.pdf', bbox_inches='tight')
+fig.savefig('out/beta_hist.png', bbox_inches='tight')
+plt.close()
+
+for s in beta_log:
+    valid = beta_log[s][~np.isnan(beta_log[s])]
+    fig, ax = plt.subplots()
+    fig.set_size_inches(5, 3)
+    ax.hist(valid, bins=20, density=True)
+    x = np.linspace(valid.min(), valid.max(), 300)
+    y = np.exp(-(x-beta_log_stats['mean'].loc[s])**2/(2*beta_log_stats['std'].loc[s]**2))/(np.sqrt(2*np.pi)*beta_log_stats['std'].loc[s])
+    ax.plot(x, y)
+    ax.set_xlabel(r'$\log(\beta)$')
+    fig.savefig('out/'+s+'_hist.pdf', bbox_inches='tight')
+    fig.savefig('out/'+s+'_hist.png', bbox_inches='tight')
+    plt.close()
--- SIRT/main.py
+++ SIRT/main.py
@@ -3,50 +3,71 @@ import pandas as pd
 import xarray as xr
 import matplotlib.pyplot as plt
 import sirt
+from scipy.linalg import logm
 
 
 rg = np.random.default_rng([ord(c) for c in 'S535 COVID-19 Forecasting'])
 
-k = np.arange(5000)
+k = np.arange(20000)
 groups = np.array(['S', 'I', 'R'])
+locations = np.array(['AK','AL','AR','AZ','CA','CO','CT','DE','FL','GA','HI','IA','ID','IL','IN','KS','KY','LA','MA','MD','ME','MI','MN','MO','MS','MT','NC','ND','NE','NH','NJ','NM','NV','NY','OH','OK','OR','PA','RI','SC','SD','TN','TX','UT','VA','VT','WA','WI','WV','WY'], dtype=object)
 
-# times = pd.read_csv('times.csv', parse_dates=True)
-times = pd.date_range(start='2020-04-01', end='2020-07-01', freq='1D')
-data_raw = pd.read_csv('../Database/master_state_data.csv', index_col=0)
-locations = data_raw.index
+times = pd.date_range(start='2020-04-01', end='2020-08-01', freq='0.25D')
+data_raw = pd.read_csv('../Database/master_state_data.csv', index_col=1)
+data_raw.index.set_names('loc', inplace=True)
+data_raw = data_raw[data_raw.index.isin(locations)]
+
+enplanements = pd.read_csv('enplanements.csv', index_col=0, squeeze=True)[locations]
+enplanements.index.set_names('loc', inplace=True)
+transport_rate = enplanements.divide(365*data_raw['Pop'], fill_value=0)
+transport_rate += transport_rate.mean()/2
+transport = xr.DataArray(transport_rate).expand_dims({'outloc':locations}).copy()
+transport.data[transport['loc'] == transport['outloc']] = 0
+transport /= transport.sum(dim='loc')/transport_rate
+transport.data[transport['loc'] == transport['outloc']] = 1-transport_rate
+transport.data = logm(transport)
+
 pop = xr.DataArray(0, coords=[times, locations, k, groups], dims=['t', 'loc', 'k', 'group'])
 initial = pop[{'t':0}]
 initial.loc[{'group':'I'}] = xr.DataArray(data_raw['positive_04102020'], dims=['loc']).expand_dims({'k': k}, -1)
 initial.loc[{'group':'R'}] = 0  # data_raw['recovered']
 initial.loc[{'group':'S'}] = xr.DataArray(data_raw['Pop'] - data_raw['positive_04102020'], dims=['loc']).expand_dims({'k': k}, -1)
-# transport = xr.DataArray(pd.read_csv('transport.csv', index_col=0), coords=[locations, locations], dims=['loc', 'outloc'])
-# transport = xr.DataArray(0, coords=[locations, locations], dims=['loc', 'outloc'])
-initMeanS = xr.DataArray(10000*rg.pareto(3, locations.shape), coords=[locations], dims=['loc'])
-transport = sirt.genTransportMatrix(initMeanS, xr.DataArray(rg.pareto(1, locations.shape*2), coords=[locations, locations], dims=['loc', 'outloc']), dim='loc', outdim='outloc', rate=0.1)
 
-# rates = xr.Dataset(pd.read_csv('rates.csv'))
+
+logbeta = pd.read_csv('../Parameters Estimation/out.csv', usecols=[0,1,3], index_col=0)
+iRate = rg.lognormal(logbeta['mean'], logbeta['std'], size=k.shape+locations.shape*2)[..., 0].T
 
 ds = xr.Dataset({'pop': pop,
                  'transport': transport,
-                 # 'iRate': rates['infection'],
-                 # 'rRate': rates['recovery'],
-                 # 'dRate': rates['death']})
-                 'iRate': xr.DataArray(rg.lognormal(-2.065, 0.99, locations.shape), dims=['loc']),
-                 'rRate': xr.DataArray(rg.lognormal(np.log(0.1), 1, locations.shape), dims=['loc']),
-                 'dRate': xr.DataArray(rg.beta(0.01, 0.99, locations.shape), dims=['loc'])})
+                 'iRate': xr.DataArray(iRate, dims=['loc', 'k']),
+                 'rRate': xr.DataArray(rg.lognormal(np.log(0.1), 0.1, locations.shape+k.shape), dims=['loc', 'k']),
+                 'dRate': xr.DataArray(rg.beta(0.01, 0.99, locations.shape+k.shape), dims=['loc', 'k'])})
 
 for i in range(len(times)-1):
     sirt.step(ds, times[i], times[i+1], rg=rg)
 
 totpop = ds.pop.sum(dim='loc')
 
-# fig, (ax1, ax2) = plt.subplots(1, 2)
-fig, ax1 = plt.subplots()
-fig.set_size_inches(5, 3)
-ax1.plot(times, totpop.loc[{'group':'I'}], color='black', alpha=0.02)
+fig, ax = plt.subplots()
+fig.set_size_inches(9, 5)
+ax.plot(times, totpop.loc[{'group':'I'}], color='black', alpha=1/255, linewidth=0.5)
 fig.autofmt_xdate()
 plt.ylabel('Cases')
-# ax2.set_yscale('log')
-# ax2.plot(times, totpop.loc[{'group':'I'}], color='black', alpha=0.03)
-plt.savefig('example-trajectories.pdf', bbox_inches='tight')
-plt.savefig('example-trajectories.svg', bbox_inches='tight')
+fig.savefig('infected.pdf', bbox_inches='tight')
+fig.savefig('infected.png', bbox_inches='tight', dpi=72*4)
+ax.set_yscale('log')
+fig.savefig('infected.log.pdf', bbox_inches='tight')
+fig.savefig('infected.log.png', bbox_inches='tight', dpi=72*4)
+plt.close()
+
+fig, ax = plt.subplots()
+fig.set_size_inches(9, 5)
+ax.plot(times, (totpop.sum(dim='group').loc[{'t':times[0]}] - totpop.sum(dim='group')).T, color='black', alpha=1/255, linewidth=0.5)
+fig.autofmt_xdate()
+plt.ylabel('Cases')
+fig.savefig('deaths.pdf', bbox_inches='tight')
+fig.savefig('deaths.png', bbox_inches='tight', dpi=72*4)
+ax.set_yscale('log')
+fig.savefig('deaths.log.pdf', bbox_inches='tight')
+fig.savefig('deaths.log.png', bbox_inches='tight', dpi=72*4)
+plt.close()
